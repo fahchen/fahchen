e95e5ef19bb07e430a84966f252c15c9e846d13d
Phil Chen <06fahchen@gmail.com>
2024-10-07 19:42:12 +0800

    docs: update about me

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 import { BlogPosts } from 'app/components/posts'
 
+const aboutMe = "I’m a full-stack software engineer with a primary focus on Elixir and TypeScript, along with related frameworks and tools. I’m currently writing some Rust code too. I love building products and contributing to open source, and I actively contribute to the open source community."
+
 export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        {`Hi, I'm Phil Chen.`}
+        {`Hi, I'm Phil Chen (fahchen).`}
       </h1>
-      <p className="mb-4">
-        {`I’m a full-stack software engineer, with a primary focus on Elixir and TypeScript, and related frameworks and tools. I love building products and open source, and actively contribute to the open source community.`}
-      </p>
+      <p className="mb-4">{aboutMe}</p>
       <div className="my-8">
         <BlogPosts />
       </div>
